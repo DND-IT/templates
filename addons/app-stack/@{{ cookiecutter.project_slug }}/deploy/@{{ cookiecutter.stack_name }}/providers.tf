a97c76aa92c512015a6552b2044c35dbec3c27a8
Roland Bapst <roland.bapst@tamedia.ch>
2025-06-23 17:15:02 +0200

    test+

---
+++ addons/app-stack/@{{ cookiecutter.project_slug }}/deploy/@{{ cookiecutter.stack_name }}/providers.tf
@@ -0,0 +1,32 @@
+locals {
+  region = "@{{ cookiecutter.aws_region }}"
+}
+
+terraform {
+  required_version = "~> 1.11.0"
+
+  backend "s3" {
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+
+  default_tags {
+    tags = {
+      Terraform   = "true"
+      Environment = var.environment
+      GithubRepo  = var.github_repo
+      GithubOrg   = var.github_org
+    }
+  }
+}
---
+++ addons/app-stack/README.md
@@ -0,0 +1,10 @@
+# Generic Stack for Application Repo
+
+This template is tailored to create a terraform stack in an application directory
+
+In short it allows to simply create:
+
+- deploy/YOURNAME terraform root directory
+- deploy/YOURNAME/environments/dev.s3.tfbackend
+- deploy/YOURNAME/environments/dev.tfvars
+- .github/workflows/YOURNAME.yaml (standard terraform apply workflow)
